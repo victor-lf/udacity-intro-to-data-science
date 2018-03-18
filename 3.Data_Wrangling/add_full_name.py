9e206811b3025069ce93d2d19509651c5c1deaf3
victor-lf <victor-lf@hotmail.com>
2018-03-18 20:31:26 -0300

    Update and rename a.txt to add_full_name.py

---
+++ 3.Data_Wrangling/add_full_name.py
@@ -0,0 +1,33 @@
+import pandas
+
+def add_full_name(path_to_csv, path_to_new_csv):
+    #Assume you will be reading in a csv file with the same columns that the
+    #Lahman baseball data set has -- most importantly, there are columns
+    #called 'nameFirst' and 'nameLast'.
+    #1) Write a function that reads a csv
+    #located at "path_to_csv" into a pandas dataframe and adds a new column
+    #called 'nameFull' with a player's full name.
+    #
+    #For example:
+    #   for Hank Aaron, nameFull would be 'Hank Aaron', 
+	#
+	#2) Write the data in the pandas dataFrame to a new csv file located at
+	#path_to_new_csv
+
+    # read csv into Pandas dataframe and add new column called 'nameFull' with a player's full name  
+    baseball_data = pandas.read_csv(path_to_csv)
+    baseball_data['nameFull'] = baseball_data.nameFirst + ' ' + baseball_data.nameLast
+    
+    # write the data in the pandas dataframe to a new csv file
+    baseball_data.to_csv(path_to_new_csv)
+    
+
+if __name__ == "__main__":
+    # For local use only
+    # If you are running this on your own machine add the path to the
+    # Lahman baseball csv and a path for the new csv.
+    # The dataset can be downloaded from this website: http://www.seanlahman.com/baseball-archive/statistics
+    # We are using the file Master.csv
+    path_to_csv = "github_repositories/udacity-intro-to-data-science/3.Data_Wrangling/baseballdatabank-2017.1/core/Master.csv"
+    path_to_new_csv = "./New_Master.csv"
+    add_full_name(path_to_csv, path_to_new_csv)
